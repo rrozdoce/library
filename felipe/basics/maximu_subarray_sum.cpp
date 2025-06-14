97e3a4a712bf6893e19925c49d8b3877f682aa27
rrozdoce <felipefelipevilhalva@gmail.com>
2025-06-14 02:30:48 -0400

    .

---
+++ felipe/basics/maximu_subarray_sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// O(n)
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int best = 0, sum = 0;
+  for(int k = 0; k < n; k++)
+  {
+    sum = mmax(array[k], sum+array[k]);
+    best = max(best, sum);
+  }
+
+  cout << best << "\n";
+  return;
+}
