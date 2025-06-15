1b53f03b4d1773c34b9bb152186f8178f74b1d0a
rrozdoce <felipefelipevilhalva@gmail.com>
2025-06-15 11:42:34 -0400

    .

---
+++ felipe/basics/binary_search.cpp
@@ -0,0 +1,11 @@
+void search(int array[], int n)
+{
+  int k = 0;
+  for(int b = n/2; b >= 1; b /= 2){
+    while(k+b < n && array[k+b] <= x) k += b;
+  }
+
+  if(array[k] == x){
+    // x found at index x
+  }
+}
